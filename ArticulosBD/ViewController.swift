f435dad2873d3311aa70c94ef7dcb7eaff76daef
Antonio Galvan <kostalosh@gmail.com>
2021-02-21 22:27:59 +0100

    Añadidos los pods de Firebase Base de datos.

--- ArticulosBD/ViewController.swift
+++ ArticulosBD/ViewController.swift
@@ -7,15 +7,21 @@
 
 import UIKit
 import FirebaseAnalytics
+import Firebase
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var txtNombre: UITextField!
+    @IBOutlet weak var txtUnidades: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         Analytics.logEvent("Inicio", parameters: ["Mensaje":"Aplicación iniciada"])
     }
 
-
+    @IBAction func btnAñadirPressed(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
 }
 
